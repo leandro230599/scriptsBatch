41e2ea22cc2119e51ca65cd6e1e353ffe29b7aff
Leandro López <51353353+leandro230599@users.noreply.github.com>
2023-04-17 23:59:03 -0300

    Add comments, loop infinite and change program

--- proPresentar.bat
+++ proPresentar.bat
@@ -1,17 +1,25 @@
 @echo off
-tasklist /fi "ImageName eq msedge.exe" /fo csv 2>NUL | find /I "msedge.exe">NUL
+:loop
+echo Script que ahorra el tener que abrir el administrador de tareas para finalizar el ProPresenter y volver a ejecutarlo
+echo --------------------------------------------------------------------------------------------------------------------
+tasklist /fi "ImageName eq ProPresenter.exe" /fo csv 2>NUL | find /I "ProPresenter.exe">NUL
 if "%ERRORLEVEL%"=="0" ( GOTO CLOSEANDSTART) else ( GOTO START)
 :AFTER
 pause
+cls
+GOTO loop
 exit
 
 :CLOSEANDSTART
 echo Programa ejecutandose, procediendo a cerrarlo...
-taskkill /f /im msedge.exe
-start msedge.exe
+taskkill /f /im ProPresenter.exe
+echo Programa cerrado, procediendo a abrirlo
+::Aqui se debe añadir la ruta del archivo exe del ProPresenter, no del acceso directo
+start C:\"Program Files"\"Renewed Vision"\ProPresenter\ProPresenter.exe
 GOTO AFTER
 
 :START
 echo Programa apagado, abriendolo...
-start msedge.exe
+::Aqui se debe añadir la ruta del archivo exe del ProPresenter, no del acceso directo
+start C:\"Program Files"\"Renewed Vision"\ProPresenter\ProPresenter.exe
 GOTO AFTER
